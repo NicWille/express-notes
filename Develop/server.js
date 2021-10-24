8be039efe0b99ac0705088e7eaabd556315fc5c8
NicWille <nic.wille1020@yahoo.com>
2021-10-24 12:54:45 -0700

    make it better

--- Develop/server.js
+++ Develop/server.js
@@ -3,13 +3,47 @@ const app = express()
 
 const PORT = process.env.PORT || 3003
 
+const path = require('path');
+const fs = require('fs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+const {notes} = require('./db/db.json')
+
+const createNewNote = function(body, notes) {
+    const note = body;
+    notes.note;
+    fs.writeFileSync(
+      path.join(__dirname, '../data/notes.json'),
+      JSON.stringify({ notes }, null, 2)
+    );
+    return note;
+  }
+app.get('/api/notes', (req, res) => {
+    console.log(notes)
+    return res.json(notes)
+})
 
 app.get('/notes', (req, res) => {
-    console.log("these are notes file")
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.post('/notes', (req, res) => {
+
+    // req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+    // res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.delete('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
 app.get('*', (req, res) => {
-    console.log("this is index file")
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 app.listen(PORT, () => {
